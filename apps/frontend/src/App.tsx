798b184577995b439837394c144d68fb09434e5f
Martin Kohoutek <martinkohoutek@hotmail.com>
2025-11-30 10:29:40 +0100

    chore(setup): udpate apps

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 function App() {
-  const [message, setMessage] = useState<string>('');
+  const [message, setMessage] = useState<string>("");
 
   useEffect(() => {
-    fetch('http://localhost:8080/api/hello')
-      .then(res => res.text())
-      .then(data => setMessage(data))
-      .catch(err => setMessage('Error: ' + err.message));
+    fetch("http://localhost:8080/api/hello")
+      .then((res) => res.text())
+      .then((data) => setMessage(data))
+      .catch((err) => setMessage("Error: " + err.message));
   }, []);
 
   return (
